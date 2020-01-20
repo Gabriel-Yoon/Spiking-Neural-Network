04b6ced7a7e1fba82ec6b21b4252c7e6b6caa236
Gabriel Yoon <53244335+Gabriel-Yoon@users.noreply.github.com>
2020-01-20 17:32:17 +0900

    Merged into one file

--- neurons/LIFNeuron.py
+++ neurons/LIFNeuron.py
@@ -1,17 +1,26 @@
-"""
-Create a basic LIF neuron class
-
-Based on http://neurdon.wpengine.com/2011/01/19/neural-modeling-with-python-part-1/
-
-- Some changes to the original model:
-- This class models the state of the neuron over time
-- The values have been modified to make the simulation work better (not based on any biological plausability)
-- Debugging has been added
-
-Requires Python 3.x and Numpy
-"""
-
 import numpy as np
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"""
+<Basic parameters>
+"""
+
+T       = 50 # total time to simulate (in msec)
+dt      = 0.0125 # simulation timestep
+time    = int(T/dt) #number of timesteps in the total simulation time
+inpt    = 1.0 #Neuron input voltage (in V)
+neuron_input = np.full((time),inpt)
+
+num_layers  = 2
+num_neurons = 100
+
+
+"""
+-----------------------------------------------------------------------------------------------------------------------
+START : LIF neuron model
+"""
 
 class LIFNeuron():
     def __init__(self, neuron_label = "LIF", debug=True):
@@ -20,18 +29,15 @@ class LIFNeuron():
         self.t_rest = 0  # initial refractory time
 
         # LIF Properties
-        self.Vm = np.array([0])  # Neuron potential (mV)
-        self.time = np.array([0])  # Time duration for the neuron (needed?)
-        self.spikes = np.array([0])  # Output (spikes) for the neuron
+        self.Vm     = np.array([0])         # Neuron potential (mV)
+        self.time   = np.array([0])       # Time duration for the neuron (needed?)
+        self.spikes = np.array([0])     # Output (spikes) for the neuron
 
-        self.t = 0  # Neuron time step
-        self.Rm = 1  # Resistance (kOhm)
-        self.Cm = 10  # Capacitance (uF)
-        self.tau_m = self.Rm * self.Cm  # Time constant
-        self.tau_ref = 4  # refractory period (ms)
-        self.Vth = 0.75  # = 1  #spike threshold
-        self.V_spike = 1  # spike delta (V)
-        self.type = 'Leaky Integrate and Fire'
+        self.type   = 'Leaky Integrate and Fire'
+        self.state  = 'active'
+        self.t_ref  = 4    # refractory period (ms)
+        self.Vth    = 0.75  # = 1  #spike threshold
+        self.V_spike = 1    # spike delta (V)
         self.neuron_label = neuron_label
         self.debug = debug
         if self.debug:
@@ -82,3 +88,125 @@ class LIFNeuron():
                    .format(self.neuron_label, self.Vm.shape, i, self.t))
 
             # return time, Vm, output
+
+"""
+END : LIF Neuron model
+-----------------------------------------------------------------------------------------------------------------------
+"""
+
+"""
+START : Create neural network
+-----------------------------------------------------------------------------------------------------------------------
+"""
+
+def create_neurons(num_layers, num_neurons, debug=True):
+    neurons = []
+    for layer in range(num_layers):
+        if debug:
+            print ('create_neurons() : Create layer{}'. format(layer))
+        neuron_layer=[]
+        for count in range(num_neurons):
+            neuron_layer.append(LIFNeuron(debug=debug))
+        neurons.append(neuron_layer)
+    return neurons
+
+"""
+END : Create neural network
+-----------------------------------------------------------------------------------------------------------------------
+"""
+
+"""
+-----------------------------------------------------------------------------------------------------------------------
+START : MNIST IMAGE_UTILS
+Render the image in each of it's retinal 'zones'
+This will be the basis of what each retinal unit views as we progress.
+Not that the pixels that will have the strongest stimuli are white (as they are closer to 1), areas of least stimuli are black (value close to 0).
+"""
+from mnist import MNIST
+mndata = MNIST('./mnist')
+images, labels = mndata.load_training()
+
+
+def get_next_image(index=0, pick_random = False, display=True):
+    if pick_random:
+        index = random.randint(0, len(images)-1)
+    image = images[index]
+    label = labels[index]
+    if display:
+        print('Label: {}'.format(label))
+        print(mndata.display(image))
+    image = np.asarray(image).reshape((28,28))
+    image_norm = (image * 255.0/image.max()) / 255.
+    return image_norm, label
+
+
+def graph_retinal_image(image, stride):
+    fig = plt.figure()
+
+    len_x, len_y = image.shape
+    x_max = int(len_x/stride[0])
+    y_max = int(len_y/stride[0])
+    print('Convolution Dimensions: x={} / y={}'.format(x_max, y_max))
+    x_count, y_count = 1, 1
+
+    for y in range (0, len_y, stride[0]):
+        x_count = 1
+        for x in range(0, len_x, stride[0]):
+            x_end = x + stride[0]
+            y_end = y + stride[0]
+            kernel = image[y:y_end, x:x_end]
+            #orientation = s1(kernel)
+            a = fig.add_subplot(y_max, x_max, (y_count-1)*x_max+x_count)
+            a.axis('off')
+            plt.imshow(kernel, cmap="gray")
+            x_count += 1
+        y_count += 1
+    plt.show()
+
+"""
+END : MNIST IMAGE_UTILS
+-----------------------------------------------------------------------------------------------------------------------
+"""
+
+"""
+-----------------------------------------------------------------------------------------------------------------------
+START : GRAPH_RESULTS
+Utility functions
+These functions are used to graph the results of spikes, membrane potential, etc for neurons in the simulation.
+"""
+def plot_neuron_behaviour(time, data, neuron_type, neuron_id, y_title):
+    plt.plot(time,data)
+    plt.title('{} @ {}'.format(neuron_type, neuron_id))
+    plt.ylabel(y_title)
+    plt.xlabel('Time (msec)')
+    # Autoscale y-axis based on the data (is this needed??)
+    y_min = 0
+    y_max = max(data)*1.2
+    if y_max == 0:
+        y_max = 1
+    plt.ylim([y_min,y_max])
+    plt.show()
+
+def plot_membrane_potential(time, Vm, neuron_type, neuron_id=0):
+    plot_neuron_behaviour(time, Vm, neuron_type, neuron_id, y_title='Membrane potential (V)')
+
+def plot_spikes(time, Vm, neuron_type, neuron_id=0):
+    plot_neuron_behaviour(time, Vm, neuron_type, neuron_id, y_title='Spike (V)')
+
+"""
+END : GRAPH_RESULTS
+-----------------------------------------------------------------------------------------------------------------------
+"""
+
+neurons = create_neurons(num_layers, num_neurons, debug=False)
+
+stimulus_len = len(neuron_input)
+layer = 0
+for neuron in range(num_neurons):
+    offset = random.randint(0,100)  #Simulates stimulus starting at different times
+    stimulus = np.zeros_like(neuron_input)
+    stimulus[offset:stimulus_len] = neuron_input[0:stimulus_len - offset]
+    neurons[layer][neuron].spike_generator(stimulus)
+
+plot_membrane_potential(neurons[0][0].time, neurons[0][0].Vm, 'Membrane Potential of {}'.format(neurons[0][0].type), neuron_id = "0/0")
+plot_spikes(neurons[0][0].time, neurons[0][0].spikes, 'Output spikes for {}'.format(neurons[0][0].type), neuron_id = "0/0")
